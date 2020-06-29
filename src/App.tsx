fb6e6f4b8f1fe8772bda94df839140cf61367fec
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-29 00:40:07 -0300

    creating a simplified auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-
 import GlobalStyle from './styles/global';
+
 // import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
+    <AuthContext.Provider value={{ name: 'Ruan' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
-    <SignIn />
   </>
 );
 
