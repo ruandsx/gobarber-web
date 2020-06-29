d3a5ece34f07a9b0c630d476d960a2d96cd2fcc1
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-28 23:07:48 -0300

    usability in the inputs

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,12 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  background: #232129;
-  border: 2px solid #232129;
-  border-radius: 10px;
-  padding: 16px;
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   width: 100%;
+  padding: 16px;
+  border-radius: 10px;
+  background: #232129;
+
+  border: 2px solid #232129;
   color: #666360;
+  transition: border-color 0.3s, color 0.3s;
 
   display: flex;
   align-items: center;
@@ -14,6 +21,19 @@ export const Container = styled.div`
   & + div {
     margin-top: 8px;
   }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
