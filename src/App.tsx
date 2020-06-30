fd564fa0759e27ad5be63ede187754d42bec6b1e
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-29 23:11:05 -0300

    connecting api into auth context

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import GlobalStyle from './styles/global';
 // import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Ruan' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
