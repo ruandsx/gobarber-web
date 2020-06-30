e564d189856b91d5e840d143faea364d4a895296
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-30 00:13:20 -0300

    renaming hooks

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import ToastContainer from './components/ToastContainer';
 
-import { AuthProvider } from './hooks/AuthContext';
+import { AuthProvider } from './hooks/auth';
+import { ToastProvider } from './hooks/toast';
 
 const App: React.FC = () => (
   <>
