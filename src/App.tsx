9f9c8700214f42a4b83dd41a4c076def16215e36
Ruan Xavier <ruandsxavier@gmail.com>
2020-06-29 23:34:50 -0300

    creating signOut method

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GlobalStyle from './styles/global';
 // import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
