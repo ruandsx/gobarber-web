40d47946c6b4dd38dfc63c97cda7f04a0c8cdf30
Ruan Xavier <ruandsxavier@gmail.com>
2020-07-29 18:18:27 -0300

    creating forgot-password route and page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,16 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
 
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
+    <Route path="/signin" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
