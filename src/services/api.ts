56290dd899aa290d426c47fd6e1f4fc1a290860c
Ruan Xavier <ruandsxavier@gmail.com>
2020-08-07 00:21:55 -0300

    updating api to use .env variables

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3333',
-  baseURL: 'http://192.168.0.35:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
